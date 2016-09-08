6851e46d6f9a9efeb7832441342890c35517db03
David James <david.james@phishme.com>
2016-09-08 10:19:10 -0400

    Testsgit add test/

--- test/integration/default/configure_spec.rb
+++ test/integration/default/configure_spec.rb
@@ -1,6 +1,10 @@
-describe file('/etc/newrelic/nrsysmond.cfg') do
-  it { should be_file }
-  it { should be_readable }
-  it { should be_writable }
-  it { should be_owned_by 'root' }
+control 'NewRelic Sysmond Config' do
+  title 'Check newrelic-sysmod config file existence'
+  desc ' Ensure the config file is a file, readable, writable and owned by root'
+  describe file('/etc/newrelic/nrsysmond.cfg') do
+    it { should be_file }
+    it { should be_readable }
+    it { should be_writable }
+    it { should be_owned_by 'root' }
+  end
 end
--- test/integration/default/install_spec.rb
+++ test/integration/default/install_spec.rb
@@ -1,3 +1,7 @@
-describe package('newrelic-sysmond') do
-      it { should be_installed }
+control 'NewRelic Sysmond Package' do
+  title 'Check newrelic-sysmod installed'
+  desc 'Ensure that newrelic-sysmond is installed.'
+  describe package('newrelic-sysmond') do
+    it { should be_installed }
+  end
 end
